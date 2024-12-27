658c42b951605004a6b476c6bf072606a791698a
CarterH93 <Carterhawkins93@gmail.com>
2024-12-26 20:49:42 -0500

    Initial Commit

---
+++ MVVM Template/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MVVM Template
+//
+//  Created by Carter Hawkins on 12/26/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MVVM Template/MVVM_TemplateApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVM_TemplateApp.swift
+//  MVVM Template
+//
+//  Created by Carter Hawkins on 12/26/24.
+//
+
+import SwiftUI
+
+@main
+struct MVVM_TemplateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
